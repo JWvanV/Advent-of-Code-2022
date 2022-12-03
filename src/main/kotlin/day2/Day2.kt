7beef611c695e4d5de26bf3ed08a6c0c43ff228a
Jan-Willem van Velzen <jan-willem.van.velzen@postnl.nl>
2022-12-03 16:51:50 +0100

    Add template and prepare for day2

---
+++ src/main/kotlin/day2/Day2.kt
@@ -0,0 +1,19 @@
+package day2
+
+import common.Puzzle
+
+fun main() {
+    puzzle.solve()
+}
+
+val puzzle = object : Puzzle(2, "example") {
+    override fun part1(): Any {
+        // TODO
+        return Unit
+    }
+
+    override fun part2(): Any {
+        // TODO
+        return Unit
+    }
+}
---
+++ src/main/kotlin/template/Day.kt
@@ -0,0 +1,19 @@
+package template
+
+import common.Puzzle
+
+fun main() {
+    puzzle.solve()
+}
+
+val puzzle = object : Puzzle(0, "example") {
+    override fun part1(): Any {
+        // TODO
+        return Unit
+    }
+
+    override fun part2(): Any {
+        // TODO
+        return Unit
+    }
+}
